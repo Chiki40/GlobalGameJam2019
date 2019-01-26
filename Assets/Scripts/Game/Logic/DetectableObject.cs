9dea4eaf46a0ff7ab15093b5cb055518207f437b
Chiki40 <chiki4089@gmail.com>
2019-01-26 11:41:37 +0100

    Object detection per object.

--- Assets/Scripts/Game/Logic/DetectableObject.cs
+++ Assets/Scripts/Game/Logic/DetectableObject.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DetectableObject : MonoBehaviour
 {
+    public float fDetectionAngleOffset = 8.0f;
+    public float fDetectionTimeLookingAtObject = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
