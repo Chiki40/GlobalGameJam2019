d12a96f16bda70ab99b4dbefe2430e1508ecc034
Chiki40 <chiki4089@gmail.com>
2019-01-26 18:17:49 +0100

    Intro.

---
+++ Assets/Scripts/Intro/Intro.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    public float m_fTimeInIntro = 5.0f;
+
+    private float m_fCurrentTimeInIntro = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_fCurrentTimeInIntro = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+
+        m_fCurrentTimeInIntro = Mathf.Min(m_fCurrentTimeInIntro + Time.deltaTime, m_fTimeInIntro);
+        if (m_fCurrentTimeInIntro >= m_fTimeInIntro)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
