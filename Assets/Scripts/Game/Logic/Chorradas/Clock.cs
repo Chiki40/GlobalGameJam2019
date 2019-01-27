dd1c402099cd092281c0d3c6619e95856bd93ca9
Chiki40 <chiki4089@gmail.com>
2019-01-27 17:44:43 +0100

    Fixed clock

--- Assets/Scripts/Game/Logic/Chorradas/Clock.cs
+++ Assets/Scripts/Game/Logic/Chorradas/Clock.cs
@@ -16,8 +16,8 @@ public class Clock : MonoBehaviour
     void Update()
     {
         DateTime time = DateTime.Now;
-        //Debug.Log(time.Hour + " " + time.Minute);
-        float hourLoopCompletionRate = (float)(time.Hour % 12) / 12.0f;
+        Debug.Log(time.Hour + " " + time.Minute);
+        float hourLoopCompletionRate = (float)(time.Hour % 12) / 13.0f;
         float minuteLoopCompletionRate = (float)(time.Minute) / 60.0f;
         Vector3 rotation = clockHours.transform.localEulerAngles;
         clockHours.transform.localEulerAngles = new Vector3(rotation.x, rotation.y, hourLoopCompletionRate * 360.0f);
