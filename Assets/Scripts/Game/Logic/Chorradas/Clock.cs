8295d94a1ecc9351afabb5a7a540835eb37f4527
CHIKI-PC\Daniel <chiki4089@gmail.com>
2019-01-28 22:45:25 +0100

    AFTER JAM: Text bug and clock bug.

--- Assets/Scripts/Game/Logic/Chorradas/Clock.cs
+++ Assets/Scripts/Game/Logic/Chorradas/Clock.cs
@@ -15,9 +15,11 @@ public class Clock : MonoBehaviour
 
     void Update()
     {
-        DateTime time = DateTime.Now;
-        Debug.Log(time.Hour + " " + time.Minute);
-        float hourLoopCompletionRate = (float)(time.Hour % 12) / 13.0f;
+        TimeZoneInfo.ClearCachedData();
+        DateTime UTCNow = DateTime.UtcNow;
+        TimeSpan offset = TimeZoneInfo.Local.BaseUtcOffset;
+        DateTime time = UTCNow + offset;
+        float hourLoopCompletionRate = (float)(time.Hour % 12) / 12.0f;
         float minuteLoopCompletionRate = (float)(time.Minute) / 60.0f;
         Vector3 rotation = clockHours.transform.localEulerAngles;
         clockHours.transform.localEulerAngles = new Vector3(rotation.x, rotation.y, hourLoopCompletionRate * 360.0f);
